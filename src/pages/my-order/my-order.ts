fd91c52cc38bb7f318283a00570481614ba652e6
Gokul B S <gokul.b@cybosol.in>
2017-12-20 18:29:53 +0530

    Added My Orders

---
+++ src/pages/my-order/my-order.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the PaymentPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-my-order',
+  templateUrl: 'my-order.html',
+})
+export class MyOrderPage {
+  public count: any;
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.count = [
+      { id: 1 },
+      { id: 2 },
+      { id: 3 },
+      { id: 4 },
+      { id: 5 },
+      { id: 6 },
+      { id: 7 },
+      { id: 8 }
+    ];
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad PaymentPage');
+  }
+
+}
